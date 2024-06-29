b274e4450abf6845534ba99a29ff2d5b222fef35
Мария <masha2355@gmail.com>
2024-06-29 22:04:10 +1000

    base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,34 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
+function solveEquation(a,b,c) {
+  let arr=[];
+  let d = (b**2-4*a*c);
+  if (d>0){
+   let root1 = (-b + Math.sqrt(d) )/(2*a);
+   let root2 = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(root1,root2);
+    console.log(arr);
+    return arr;
+  }
+if (d === 0){
+  let root0= -b/(2*a);
+  arr.push(root0);
+    console.log(arr);
   return arr;
+}
+else {
+  console.log(arr)
+}
+  return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent/100/12;
+
+  let credBody = amount - contribution;
+  let monthlyPayment = credBody * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
+  let totalPayment = monthlyPayment * countMonths;
+
+  totalPayment = Number(totalPayment.toFixed(2));
+
+  return totalPayment;
 }
